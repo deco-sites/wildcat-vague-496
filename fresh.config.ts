38b31f60d1c35adcc11d4310e662065428e1da49
Tiago Gimenes <gimenes@deco.cx>
2024-03-27 18:53:55 +0000

    Let's get started!

---
+++ fresh.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "$fresh/server.ts";
+import plugins from "https://denopkg.com/deco-sites/std@1.22.0/plugins/mod.ts";
+import decoManifest from "./manifest.gen.ts";
+
+export default defineConfig({
+  plugins: plugins({
+    manifest: decoManifest,
+  }),
+});
